9cb921a75134b43b4d52862b85b3deea1330e731
CWallbridge <Chris.Wallbridge@googlemail.com>
2018-05-23 09:53:14 +0100

    Simple spatial descriptions added. Some initial tests have been added, more to follow.

---
+++ src/spatial_desc.py
@@ -0,0 +1,163 @@
+import os
+import sys
+import math
+import rospy
+import numpy
+import geometry_msgs.msg
+import enchant
+
+import underworlds
+from underworlds.tools.spatial_relations import *
+
+import logging; logger = logging.getLogger("spatial_description")
+
+def isin(lang="en_GB"):
+
+    if lang=="en_GB":
+        return "in"
+    else:
+        raise NotImplementedError
+        
+def isontop(lang="en_GB"):
+
+    if lang=="en_GB":
+        return "on top of"
+    else:
+        raise NotImplementedError
+        
+def isabove(lang="en_GB"):
+
+    if lang=="en_GB":
+        return "above"
+    else:
+        raise NotImplementedError
+        
+def isclose(lang="en_GB"):
+
+    if lang=="en_GB":
+        return "close to"
+    else:
+        raise NotImplementedError
+        
+def istonorth(lang="en_GB"):
+
+    if lang=="en_GB":
+        return "to the north of"
+    else:
+        raise NotImplementedError
+        
+def istoeast(lang="en_GB"):
+
+    if lang=="en_GB":
+        return "to the east of"
+    else:
+        raise NotImplementedError
+        
+def istosouth(lang="en_GB"):
+
+    if lang=="en_GB":
+        return "to the south of"
+    else:
+        raise NotImplementedError
+        
+def istowest(lang="en_GB"):
+
+    if lang=="en_GB":
+        return "to the west of"
+    else:
+        raise NotImplementedError
+
+def get_desc_relation(relation, lang="en_GB"):
+    
+    if relation == "in":
+        return isin(lang)
+        
+    elif relation == "onTop":
+        return isontop(lang)
+        
+    elif relation == "above":
+        return isabove(lang)
+        
+    elif relation == "close":
+        return isclose(lang)
+        
+    elif relation == "toNorth":
+        return istonorth(lang)
+        
+    elif relation == "toEast":
+        return istoeast(lang)
+        
+    elif relation == "toSouth":
+        return istosouth(lang)
+        
+    elif relation == "toWest":
+        return istowest(lang)
+        
+    else:
+        raise NotImplementedError
+
+def add_noun_article(noun, amount, lang="en_GB"):
+    
+    d = enchant.Dict(lang)
+    
+    if noun[0].isupper() or d.check(noun) == False:
+        #assume that it is a proper noun
+        return noun
+    elif lang=="en_GB":
+        #regular noun
+        vowels = ["a", "e", "i", "o", "u"]
+        if amount == 1:
+            return "the %s" % noun
+        else:
+            if noun[0] in vowels:
+                return "an %s" % noun
+            else:
+                return "a %s" % noun
+                
+    else:
+        raise NotImplementedError
+        
+        
+def amb_desc(worldName, rel_list, iteration, lang="en_GB"):
+    
+    with underworlds.Context("spatial_description") as ctx:
+        world = ctx.world[worldName]
+        
+        node2 = world.scene.nodes[rel_list[iteration][2]]
+        amount2 = len(world.scene.nodebyname(node2.name))
+        
+        noun2 = add_noun_article(node2.name, amount2, lang)
+        
+        sr_desc = get_desc_relation(rel_list[iteration][3], lang)
+        
+        if lang=="en_GB":
+            description = sr_desc + " " + noun2 
+        else:
+            raise NotImplementedError 
+        
+        if iteration == 0:
+            node1 = world.scene.nodes[rel_list[iteration][1]]
+            amount1 = len(world.scene.nodebyname(node1.name))
+            
+            noun1 = add_noun_article(node1.name, amount1, lang)
+            
+            description = noun1 + " is " + description
+    
+    return description
+
+def gen_spatial_desc(worldName, nodeID, lang="en_GB", descType = "Simple"):
+    
+    #Retrieve the spatial relations and sort them by priority    
+    rel_list = sorted(get_node_sr(worldName, nodeID))
+        
+    if descType == "Simple":
+        #Getting a simple one word description so only check the first relation
+        description = amb_desc(worldName, rel_list, lang, 0)
+    else:
+        raise NotImplementedError
+    
+    return description
+    
+if __name__ == "__main__":
+    
+    print("Finished")
--- testing/test_descriptions.py
+++ testing/test_descriptions.py
@@ -1,16 +1,58 @@
 import unittest
 import time
+import sys
+import os
+
+import underworlds
+import underworlds.server
+from underworlds.tools.loader import ModelLoader
+from underworlds.tools.spatial_relations import *
+
+#Included until I setup an installer
+sl = os.path.dirname(__file__)
+sys.path.append(os.path.join(sl, '..', 'src'))
+
+from spatial_desc import *
 
 #Check that the descriptions generated are the ones expected.
 
 class TestDescriptions(unittest.TestCase):
-	
-	def setUp(self):
-		pass
-	
-	def test_descriptions(self):
-		self.assertTrue(False) #Not yet implemented
-	
+    
+    # workaround for https://github.com/grpc/grpc/issues/14088
+    @classmethod
+    def setUpClass(cls):
+        cls.server = underworlds.server.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.server.stop(1).wait()
+
+    def setUp(self):
+        # workaround for https://github.com/grpc/grpc/issues/14088
+        #self.server = underworlds.server.start()
+
+        self.ctx = underworlds.Context("unittest - edit tools")
+
+        # workaround for https://github.com/grpc/grpc/issues/14088
+        self.ctx.reset()
+    
+    def test_simple_descriptions(self):
+        
+        worldName = "base"
+        world = self.ctx.worlds[worldName]
+        
+        ModelLoader().load("res/spatialDescSimple.blend", world=worldName)
+        time.sleep(1) # leave some time for the loader to finish
+        
+        inNode = world.scene.nodebyname("Inside")[0]
+        description = gen_spatial_desc(worldName, inNode.id, "en_GB", "Simple")
+        
+        print(description)
+        self.assertTrue(description == "Inside is in Main")
+        
+    def tearDown(self):
+        self.ctx.close()
+    
 def test_suite():
     suite = unittest.TestLoader().loadTestsFromTestCase(TestDescriptions)
     return suite
